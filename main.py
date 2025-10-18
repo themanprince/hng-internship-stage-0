2be3e81a7cbe6fe7dfce08e2335f6f42439b289d
themanprince <princeadigwe29@gmail.com>
2025-10-18 11:18:47 +0100

    rate limiting on /me route

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from dotenv import dotenv_values
 from log.logger import get_logger_middleware
 
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+limit = (dotenv_values(".env"))["RATE_LIMIT"]
 
 app = FastAPI()
 
@@ -17,5 +26,6 @@ app.middleware("http")(get_logger_middleware(log_file_path = "log/logs.txt"))
 
 
 @app.get("/me")
+@limiter.limit(limit or "60/minute")
 async def handleGetMe():
 	pass
